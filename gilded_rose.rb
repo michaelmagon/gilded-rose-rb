c6c90a95a73a6ee1bdccf2451da41c21e83db0f4
Michael Magon <michaelmagon@Michaels-MacBook-Pro.local>
2019-12-14 01:51:56 +0800

    refactor

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,54 +1,60 @@
 class GildedRose
+  BRIE = "Aged Brie"
+  BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+  SULFURAS = "Sulfuras, Hand of Ragnaros"
+  CONJURED = "Conjured Mana Cake"
+  NORMAL_DECREMENT = 1
+  CONJURED_DECREMENT = NORMAL_DECREMENT*2
+
   def initialize(items)
     @items = items
   end
 
   def update_quality()
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
+      unless item.name == SULFURAS
+        sell_by_decrement(item)
+        case item.name
+        when BRIE
+          quality_increment(item)
+          if item.sell_in < 0
+            quality_increment(item)
           end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
+        when BACKSTAGE_PASS
+          quality_increment(item)
+          if item.sell_in < 11
+            quality_increment(item)
           end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
+          if item.sell_in < 6
+            quality_increment(item)
           end
+          if item.sell_in < 0
+            item.quality -= item.quality
+          end
+        when CONJURED
+          decement_expired(item, CONJURED_DECREMENT)
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          decement_expired(item, NORMAL_DECREMENT)
         end
       end
+
     end
+  end
+
+  def quality_increment(item)
+    item.quality += 1 if item.quality < 50
+  end
+
+  def quality_decrement(item, amount)
+    amount.times{ item.quality -= 1 if item.quality > 0 }
+  end
+
+  def sell_by_decrement(item) 
+    item.sell_in -= 1
+  end
+
+  def decement_expired(item, decement)
+    item.sell_in < 0 ? quality_decrement(item,decement*2) : quality_decrement(item,decement)
   end
 end
 
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -5,7 +5,55 @@ describe GildedRose do
     it 'does not change the name' do
       items = [Item.new('foo', 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq 'fixme'
+      expect(items[0].name).to eq 'foo'
     end
+
+    it 'item sulfuras does not change' do
+      items = [Item.new('Sulfuras, Hand of Ragnaros', 10, 80)]
+      rose = GildedRose.new(items)
+      5.times{rose.update_quality()}
+      expect(items[0].sell_in).to eq 10
+      expect(items[0].quality).to eq 80
+    end    
+
+    it 'item Aged Brie quality does not decrement' do
+      items = [Item.new('Aged Brie', 5, 12)]
+      rose = GildedRose.new(items)
+      8.times{rose.update_quality()}
+      expect(items[0].quality).to be > 12
+      expect(items[0].quality).to eq 23
+    end 
+
+    it 'item Backstage passes quality does not decrement before sell by date' do
+      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 14, 13)]
+      rose = GildedRose.new(items)
+      14.times{rose.update_quality()}
+      expect(items[0].quality).to eq 44
+    end 
+
+    it 'item Backstage passes quality drops to zero after sell by date' do
+      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 12, 12)]
+      rose = GildedRose.new(items)
+      13.times{rose.update_quality()}
+      expect(items[0].quality).to eq 0
+      expect(items[0].sell_in).to eq -1
+    end 
+
+    it 'item Conjured Mana Cake quality drops twice  as fast' do
+      items = [Item.new('Conjured Mana Cake', 6, 22)]
+      rose = GildedRose.new(items)
+      10.times{rose.update_quality()}
+      expect(items[0].sell_in).to eq -4
+      expect(items[0].quality).to eq 0
+    end 
+
+    it 'normal item quality drops once per day' do
+      items = [Item.new('Elixir of the Mongoose', 6, 22)]
+      rose = GildedRose.new(items)
+      8.times{rose.update_quality()}
+      expect(items[0].sell_in).to eq -2
+      expect(items[0].quality).to eq 12
+    end 
+
   end
 end
